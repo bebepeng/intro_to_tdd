3983348c9486593114ac92ddb17ffe0818597561
Bebe Peng <bebepeng@gmail.com>
2014-03-10 18:33:02 -0600

    Removed unneccessary comments from odd_even_spec

--- spec/odd_even_spec.rb
+++ spec/odd_even_spec.rb
@@ -11,6 +11,3 @@ describe OddEven do
   end
 end
 
-#The class will return the numbers starting with 1 and ending with the number that it was initialized with when the run method is called. For example, if I pass in 10, then the program will return an array containing 1 through 10.
-#If the number is even then the program will replace the number in the array with the text "Even".
-#If the number is odd, then the program will replace the number in the array with the text "Odd".
